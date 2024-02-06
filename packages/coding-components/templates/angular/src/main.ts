23dc07efb540500e5a044b6d0bceb5b88ca1b5da
Nick Romano <nromano@esri.com>
2024-02-06 09:56:56 -0800

    chore: update coding components angular asset loading

--- packages/coding-components/templates/angular/src/main.ts
+++ packages/coding-components/templates/angular/src/main.ts
@@ -16,8 +16,11 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
+import { defineCustomElements as defineCodingElements } from "@arcgis/coding-components/dist/loader";
 
-defineCalciteElements(window, { resourcesUrl: "./assets" });
+// define custom elements in the browser, and load the assets from the CDN
+defineCalciteElements(window, { resourcesUrl: "https://js.arcgis.com/calcite-components/2.4.0/assets" });
+defineCodingElements(window, { resourcesUrl: "https://js.arcgis.com/coding-components/4.28/assets" });
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
