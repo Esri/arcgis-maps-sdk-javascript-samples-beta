5d191101767908b430e3339d06a5dba35401e1d1
Nick Romano <nromano@esri.com>
2024-02-16 17:48:22 -0800

    fix: broken vite config

--- packages/coding-components/templates/vue/vite.config.js
+++ packages/coding-components/templates/vue/vite.config.js
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
