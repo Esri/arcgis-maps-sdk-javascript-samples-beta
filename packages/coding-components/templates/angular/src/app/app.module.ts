fc6e4cebd2abe23f33ed5d5020adb1670f63d788
Nick Romano <nromano@esri.com>
2024-02-20 17:43:54 -0800

    refactor: alias ComponentLibraryModule as CodingComponentsModule

--- packages/coding-components/templates/angular/src/app/app.module.ts
+++ packages/coding-components/templates/angular/src/app/app.module.ts
@@ -18,12 +18,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 // Angular wrapper for coding components
-import { ComponentLibraryModule } from "@arcgis/coding-components-angular";
+import { ComponentLibraryModule as CodingComponentsModule } from "@arcgis/coding-components-angular";
 import { CalciteComponentsModule } from "@esri/calcite-components-angular";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ComponentLibraryModule, CalciteComponentsModule],
+  imports: [BrowserModule, CodingComponentsModule, CalciteComponentsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
