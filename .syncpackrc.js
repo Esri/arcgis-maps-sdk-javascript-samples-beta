e028b2cf96c9229e2b1750c73529a8537c9b8c69
manugadde <sai13409@esri.com>
2024-06-06 15:54:58 -0700

    Revert "chore: update dependencies"
    This reverts commit 40bddb05191e507f360ba74447e0e1a0e96935f7.

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -52,7 +52,7 @@ const config = {
       dependencies: ["@arcgis/core"],
       dependencyTypes: ["dev", "prod"],
       packages: ["**"],
-      pinVersion: ">=4.30-next <4.31"
+      pinVersion: "rc"
     },
     {
       label: "ArcGIS Components dev and prod dependency",
@@ -66,7 +66,7 @@ const config = {
         "@arcgis/map-components-react",
       ],
       packages: ["**"],
-      pinVersion: ">=4.30-next <4.31"
+      pinVersion: "rc"
     },
     {
       label: "ArcGIS Charts Components dev and prod dependency",
@@ -78,7 +78,7 @@ const config = {
         "@arcgis/charts-components-react"
       ],
       packages: ["**"],
-      pinVersion: ">=4.30-next <4.31"
+      pinVersion: "rc"
     },
     {
       label: "Calcite Components dev dependency",
